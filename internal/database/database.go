60702d2f0a61536da6ed20bc5c92590c1ce6107f
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-03-08 17:48:54 -0500

    Add common and database packages. Add config file to define environment variables and add database file to wrap around GORM and sqlite

---
+++ internal/database/database.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"github.com/skhanal5/cdl-betting/internal/common"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Database struct {
+	session *gorm.DB //potential smell
+}
+
+func New(config common.Config) (Database, error) { //TODO: Revisit parameter
+	// use env var here
+	session, err := gorm.Open(sqlite.Open(config.DBFile), &gorm.Config{})
+	if err != nil {
+		panic("Failed get a connection with the database") // todo: error handling
+	}
+	return Database{session: session}, nil
+}
